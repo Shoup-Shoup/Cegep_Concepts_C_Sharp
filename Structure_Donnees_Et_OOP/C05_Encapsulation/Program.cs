d8ee8cb6888f18d3968a79d9b191fe4f88b62084
gmari <gmari@MARILOU_PC>
2026-02-28 22:43:38 -0500

    tt

---
+++ Structure_Donnees_Et_OOP/C05_Encapsulation/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace C05_Encapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Encapsulation (avec des getters and setters):
+             */
+        }
+    }
+}
