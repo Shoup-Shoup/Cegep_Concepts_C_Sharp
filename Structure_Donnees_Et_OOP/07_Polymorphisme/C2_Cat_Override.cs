e8d6b139ceeec51e0d9fe340e5996dc570421560
gmari <gmari@MARILOU_PC>
2026-03-26 21:41:28 -0400

    LL

--- Structure_Donnees_Et_OOP/07_Polymorphisme/C2_Cat_Override.cs
+++ Structure_Donnees_Et_OOP/07_Polymorphisme/C2_Cat_Override.cs
@@ -14,6 +14,7 @@ namespace _02_Methode
          *          et de override dans la classe fille
          *      La méthode qui est override doit avoir la même signature (nom de la méthode + ses paramètres) que la méthode
          *          de la classe de base
+         *          
          */
 
         public override void Speak()
